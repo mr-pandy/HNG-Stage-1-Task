b48cdadf5d62f984d2a1bcd68802fe345b1a1e27
mr-pandy <ekerekepreciousimeh@gmail.com>
2022-11-04 22:36:34 +0100

    Completed the Form Validation

--- src/components/contactme/ContactMe.jsx
+++ src/components/contactme/ContactMe.jsx
@@ -47,49 +47,54 @@ const ContactMe = () => {
             <div className="contact__info-form-firstName">
               <label hmtlfor="first_name">First Name </label>
               <input
+              className={`${(errors.firstName && touched.firstName) ? "errorInput" :""}`}
                 id="first_name"
                 type="text"
                 placeholder="Enter your first name"
                 {...getFieldProps("firstName")}
               />
               {errors.firstName && touched.firstName && (
-                <p className="error">{errors.firstName}</p>
+                <p className="errorText">{errors.firstName}</p>
               )}
             </div>
             <div className="contact__info-form-lastName">
               <label hmtlfor="last_name">Last Name </label>
               <input
+                className={`${(errors.firstName && touched.firstName) ? "errorInput" :""}`}
                 type="text"
                 placeholder="Enter your last name"
                 {...getFieldProps("lastName")}
               />
-              {errors.lastName && touched.lastName && <p>{errors.lastName}</p>}
+              {errors.lastName && touched.lastName && <p className="errorText">{errors.lastName}</p>}
             </div>
           </div>
           <div className="contact__info-form-email">
             <label hmtlfor="email">Email </label>
             <input
+              className={`${(errors.firstName && touched.firstName) ? "errorInput" :""}`}
               id="email"
               type="email"
               placeholder="yourname@email.com"
               {...getFieldProps("email")}
             />
-            {errors.email && touched.email && <p>{errors.email}</p>}
+            {errors.email && touched.email && <p className="errorText">{errors.email}</p>}
           </div>
           <div className="contact__info-form-message">
             <label hmtlfor="message">Message </label>
             <textarea
+              className={`${(errors.firstName && touched.firstName) ? "errorInput" :""}`}
               id="message"
               cols="30"
               rows="10"
               placeholder="Send me a message and I'll reply you as soon as possible..."
               {...getFieldProps("message")}
             />
-            {errors.message && touched.message && <p>{errors.message}</p>}
+            {errors.message && touched.message && <p className="errorText">{errors.message}</p>}
           </div>
           <div>
             <div className="contact__info-form-checkbox">
               <input
+                className={`${(errors.firstName && touched.firstName) ? "errorInput" :""}`}
                 type="checkbox"
                 name="checkbox"
                 id="checkbox"
@@ -100,10 +105,10 @@ const ContactMe = () => {
               />
               <label hmtlfor="checkbox">
                 You agree to providing your data to Ekereke Precious Imeh who
-                may contact you.{" "}
+                may contact you.
               </label>
             </div>
-            {errors.checkbox && touched.checkbox && <p>{errors.checkbox}</p>}
+            {errors.checkbox && touched.checkbox && <p className="errorText">{errors.checkbox}</p>}
           </div>
           <div className="contact__info-form-button">
             <button
